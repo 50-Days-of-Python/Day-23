081cebf76c8bf98b9cf1d108d54149dc017c8788
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-02-10 11:34:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import main;
 
 def test1():
-    assert main.make_tuples([1,2,3,4], [5,6,7,8]) == [(1,5), (2,6), (3,7), (4,8)]
+    assert main.simple_calculator("add", 5, 4) == 9
 def test2():
-    assert main.make_tuples([1,2.5,3,4], [5,6,7,8]) == [(1,5), (2.5,6), (3,7), (4,8)]
+    assert main.simple_calculator("multiply", 19, 15) == 285
 def test3():
-    assert main.make_tuples(['a','b','c','d'], ['e','f','g','h']) == [('a','e'), ('b','f'), ('c','g'), ('d','h')]
+    assert main.simple_calculator("division", 9, 0) == "ZeroDivisionError"
